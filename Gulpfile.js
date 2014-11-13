b892ff67cd700766a064f9df807f871dffe3e648
FakhreevMarat <marat.fakhreev@flatstack.com>
2014-11-13 23:44:30 +0300

    moved prism code from Gulpfile to appropirate task

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,15 +1,4 @@
 var coffeeScript = require('coffee-script');
 var requireDir = require('require-dir');
-var prism = require('connect-prism');
-
-prism.create({
-  name: 'serve',
-  mode: 'mock',
-  context: '/api',
-  host: 'localhost',
-  port: 8001,
-  delay: 0,
-  rewrite: {}
-});
 
 requireDir('./gulp/tasks', { recurse: true });
