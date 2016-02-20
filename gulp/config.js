587286714f299be0cf28db81217c91b7f7aa8c52
maratfakhreev <marat.fakhreev@flatstack.com>
2016-02-20 11:59:41 +0300

    Cleanup gulp config

--- gulp/config.js
+++ gulp/config.js
@@ -12,14 +12,8 @@ const gulpConfig = {
   mocksDir: 'mocks',
   configDir: 'config',
   gulpDir: 'gulp',
-  get isProduction() {
-    return this.env === 'production';
-  },
   get isDevelopment() {
     return this.env === 'development';
-  },
-  get isTest() {
-    return this.env === 'test';
   }
 };
 const config = configParser(gulpConfig.env);
